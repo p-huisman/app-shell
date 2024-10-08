d5f1af5b201ad5ce08164c4580ca1067d82a6718
Peter Huisman <peter.huisman@pggm.nl>
2024-10-08 17:32:55 +0200

    on click base path header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,6 @@
 import { makeStyles } from "@fluentui/react-components";
 import React from "react";
 
-const pathName = new URL(document.querySelector("base").href || "/").pathname;
-
 const useStyles = makeStyles({
   header: {
     gridArea: "header",
@@ -14,8 +12,8 @@ export const AppShellHeader = () => {
   const styles = useStyles();
   return (
     <header id="Header" className={styles.header}>
-        <button onClick={() => history.pushState({}, "", pathName + "oauth")}>Login</button>
-        <button onClick={() => history.pushState({}, "", pathName + "oauth/logout")}>Logout</button>
+        <button onClick={() => history.pushState({}, "",  new URL(document.querySelector("base").href || "/").pathname + "oauth")}>Login</button>
+        <button onClick={() => history.pushState({}, "",  new URL(document.querySelector("base").href || "/").pathname + "oauth/logout")}>Logout</button>
     </header>
   );
 };
