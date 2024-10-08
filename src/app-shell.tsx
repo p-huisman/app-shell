31c0bb987e52e8e169b02e91f5ea705be6365df1
Peter Huisman <peter.huisman@pggm.nl>
2024-10-08 22:11:23 +0200

    initial brand theme

--- src/app-shell.tsx
+++ src/app-shell.tsx
@@ -1,19 +1,44 @@
-import React, { useState } from "react";
-import { createRoot } from "react-dom/client.js";
+import React, {useState} from "react";
+import {createRoot} from "react-dom/client.js";
 
-import AppShellDrawer, { AppNavItem } from "./components/navDrawer";
+import AppShellDrawer, {AppNavItem} from "./components/navDrawer";
 import AppShellHeader from "./components/header";
 
 import {
+  createDarkTheme,
+  createLightTheme,
   FluentProvider,
   makeStyles,
   webLightTheme,
 } from "@fluentui/react-components";
 
+const myBrand = {
+  "10": "#000000",
+  "20": "#011800",
+  "30": "#002700",
+  "40": "#00360c",
+  "50": "#004612",
+  "60": "#005618",
+  "70": "#00671f",
+  "80": "#1d8551", 
+  "90": "#218935",
+  "100": "#3e9949",
+  "110": "#59a85e",
+  "120": "#73b776",
+  "130": "#8ec68f",
+  "140": "#aad5a9",
+  "150": "#c6e4c5",
+  "160": "#e2f2e2",
+};
+
+const light = createLightTheme(myBrand);
+const dark = createDarkTheme(myBrand);
+
 const rootNode = document.getElementById("app-shell");
 if (!rootNode) {
   throw new Error("No root element found");
 }
+
 const useStyles = makeStyles({
   main: {
     margin: 0,
@@ -29,7 +54,7 @@ const useStyles = makeStyles({
   },
 });
 
-const defaultApps: AppNavItem[] = [{ title: "Dashboard", href: "./" }];
+const defaultApps: AppNavItem[] = [{title: "Dashboard", href: "./"}];
 
 const App = () => {
   const styles = useStyles();
@@ -49,8 +74,8 @@ const App = () => {
 
 createRoot(rootNode).render(
   <React.StrictMode>
-    <FluentProvider theme={webLightTheme}>
+    <FluentProvider theme={light}>
       <App />
     </FluentProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,59 +1,97 @@
-import {makeStyles} from "@fluentui/react-components";
-import React, { useEffect, useState } from "react";
+import {makeStyles, MenuGroupHeader} from "@fluentui/react-components";
+import React, {useEffect, useState} from "react";
+import {
+  Avatar,
+  Menu,
+  MenuTrigger,
+  MenuList,
+  MenuItem,
+  MenuPopover,
+} from "@fluentui/react-components";
 
-import { getUserInfo } from "../helpers/oauth";
+import {getUserInfo} from "../helpers/oauth";
 
 const useStyles = makeStyles({
   header: {
     gridArea: "header",
     padding: "0.5em 1em 1em 1em ",
     display: "flex",
-    gap: "1em",
+    gap: ".5em",
+    justifyContent: "flex-end",
   },
-  
+  avatar: {
+    cursor: "pointer",
+  }
 });
-
-;
 
 export const AppShellHeader = () => {
   const styles = useStyles();
-  const[userName, setUserName] = useState("")
+  const [userName, setUserName] = useState("");
   useEffect(() => {
     window.addEventListener("oauthDone", () => {
-        userInfo();
+      userInfo();
     });
     userInfo();
-  }, [])
+  }, []);
 
-  async function userInfo(){
-      const userInfo = await getUserInfo();
-      setUserName(userInfo ? userInfo.name : "");
+  async function userInfo() {
+    const userInfo = await getUserInfo();
+    setUserName(userInfo ? userInfo.name : "");
   }
-  
-  
+
   return (
     <header id="Header" className={styles.header}>
-      {userName ? <span>{userName}</span>: null}
-      {userName === "" ? <a
-        href="oauth"
-        onClick={(e) => {
-          e.preventDefault();
-          const target = e.target as HTMLAnchorElement;
-          history.pushState({}, "", target.href);
-        }}
-      >
-        Login
-      </a> : <a
-        href="oauth/logout"
-        onClick={(e) => {
-          e.preventDefault();
-          const target = e.target as HTMLAnchorElement;
-          history.pushState({}, "", target.href);
-        }}
-      >
-        Logout
-      </a>}
-      
+      <Menu>
+        <MenuTrigger disableButtonEnhancement>
+          <Avatar
+            data-href="oauth"
+            className={styles.avatar}
+            aria-label={userName}
+            name={userName}
+            color="brand"
+          ></Avatar>
+        </MenuTrigger>
+        <MenuPopover>
+          <MenuList>
+            {userName !== "" ? (
+              <MenuGroupHeader>{userName}</MenuGroupHeader>
+            ) : null }
+            {userName === "" ? (
+              <MenuItem
+                data-href="oauth"
+                onClick={(e) => {
+                  const target = e.target as HTMLElement;
+                  const menuItem = target.closest(
+                    `[role="menuitem"]`,
+                  ) as HTMLElement;
+                  const link =
+                    new URL(document.querySelector("base").href).pathname +
+                    menuItem.dataset.href;
+                  history.pushState({}, "", link);
+                }}
+              >
+                Login
+              </MenuItem>
+            ) : (
+              <MenuItem
+                data-href="oauth/logout"
+                onClick={(e) => {
+                  const target = e.target as HTMLElement;
+                  const menuItem = target.closest(
+                    `[role="menuitem"]`,
+                  ) as HTMLElement;
+                  const link =
+                    new URL(document.querySelector("base").href).pathname +
+                    menuItem.dataset.href;
+                  history.pushState({}, "", link);
+                }}
+              >
+                Logout
+              </MenuItem>
+            )}
+          </MenuList>
+        </MenuPopover>
+      </Menu>
     </header>
   );
 };
