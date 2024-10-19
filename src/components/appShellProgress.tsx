5acba8ef72d01522715da10056d4fc13a677b2a9
Peter Huisman <peter.huisman@pggm.nl>
2024-10-19 07:36:50 +0200

    bestter styleing for progress bar

--- src/components/appShellProgress.tsx
+++ src/components/appShellProgress.tsx
@@ -5,40 +5,56 @@ const cs = new CSSStyleSheet();
 cs.replaceSync(`
     @keyframes indeterminateAnimation {
       0% {
-        transform:  translateX(0) scaleX(0);
+        transform:  translateX(-50%) scaleX(0);
       }
       0% {
-        transform:  translateX(0) scaleX(0.4);
+        transform:  translateX(-50%) scaleX(0.4);
       }
       100% {
-        transform:  translateX(100%) scaleX(0.5);
+        transform:  translateX(150%) scaleX(0.5);
       }
+
     }
     `);
 document.adoptedStyleSheets = [...document.adoptedStyleSheets, cs];
 
 const useStyles = makeStyles({
-  progressBar: {
-    height: "1px",
+  progressbar: {
+    height: "3px",
     width: "100%",
     overflow: "hidden",
     visibility: "hidden",
     opacity: 0,
   },
-  progresBarVisible: {
-    height: "1px",
+  progresbarVisible: {
+    height: "3px",
     backgroundColor: "var(--colorBrandBackground2)",
     width: "100%",
     overflow: "hidden",
     visibility: "visible",
-    opacity: .8,
   },
-  progressBarValue: {
+  progressbarValue: {
     width: "100%",
     height: "100%",
     backgroundColor: "var(--colorBrandBackground)",
-    animation: "indeterminateAnimation .8s infinite linear",
+    backgroundImage:
+      "linear-gradient(to right, var(--colorNeutralBackground6) 0%, var(--colorTransparentBackground) 50%, var(--colorNeutralBackground6) 100%)",
+    animation: "indeterminateAnimation 1.1s infinite linear",
     transformOrigin: "0% 50%",
+  },
+  progressbarLabel: {
+    /* */
+    // screenreader only
+    position: "absolute",
+    width: "1px",
+    height: "1px",
+    padding: 0,
+    overflow: "hidden",
+    clip: "rect(0, 0, 0, 0)",
+    whiteSpace: "nowrap",
+    border: 0,
+    margin: "-1",
+
   },
 });
 
@@ -48,13 +64,16 @@ export interface AppShellProgressProps {
 export const AppShellProgress = (props: AppShellProgressProps) => {
   const styles = useStyles();
   return (
-    <div>
+    <div role="progressbar" aria-describedby="#ProgressbarLabel" aria-hidden={props.active ? "false": "true"} >
       {props.active}
       <div
-        className={props.active ? styles.progresBarVisible : styles.progressBar}
+        className={props.active ? styles.progresbarVisible : styles.progressbar}
       >
-        <div className={styles.progressBarValue}></div>
+        <div className={styles.progressbarValue}></div>
       </div>
+      <label id="ProgressbarLabel" className={styles.progressbarLabel}>
+        Loading...
+      </label>
     </div>
   );
 };
