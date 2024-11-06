297596ac6dc9135b4cb3b648330f774e7fc3f0af
Peter Huisman <peter.huisman@pggm.nl>
2024-11-06 15:51:12 +0100

    Refactor service worker to use Route type in registerRoute and remove unused workbox-sw imports

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,12 +1,8 @@
-// import "workbox-sw";
-import {registerRoute} from 'workbox-routing';
+import {registerRoute, Route} from 'workbox-routing';
 import {StaleWhileRevalidate} from 'workbox-strategies';
 
-// importScripts(
-//   'https://storage.googleapis.com/workbox-cdn/releases/7.3.0/workbox-sw.js'
-// );
 
 registerRoute(
-  ({url}) => url.hostname.match('esm.sh'),
+  (route: Route) => route.url.hostname.match('esm.sh'),
   new StaleWhileRevalidate()
 );
